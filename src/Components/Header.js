91c8c204f1131e30f134d9904a8e94b8c6fa97eb
liwi <liwiggwp@gmail.com>
2022-06-12 18:49:45 +0500

    add header

---
+++ src/Components/Header.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react'
+import { Navbar, Nav, Form, FormControl, Container,Button } from 'react-bootstrap'
+import logo from './logo.png'
+
+export default class Header extends Component{
+    render(){
+        return(
+            <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img
+                            src={logo}
+                            height="30"
+                            width="30"
+                            className="d-inline-block align-top"
+                            alt="Logo"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/about">About</Nav.Link>
+                            <Nav.Link href="/contacts">Contacts</Nav.Link>
+                            <Nav.Link href="/blog">Blog</Nav.Link>
+                        </Nav>
+                        <Form className="d-flex">
+                            <FormControl
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                            />
+                            <Button variant="outline-info">Search</Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        )
+    }
+}
